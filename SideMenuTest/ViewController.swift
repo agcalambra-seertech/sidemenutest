a3bcdc7f5f0acbfc0c245eb54d65c26cc3c06b1c
Allister Alambra <allisteralambra@Allisters-MacBook-Pro.local>
2016-11-16 16:00:25 +0800

    Initial Commit

---
+++ SideMenuTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideMenuTest
+//
+//  Created by Allister Alambra on 16/11/2016.
+//  Copyright © 2016 Allister Alambra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
